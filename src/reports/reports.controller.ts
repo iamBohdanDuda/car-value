616f2226cc34bb0eea280a36a6e9fc46f4923823
Bohdan <bohdan.duda.it@gmail.com>
2024-02-17 13:03:47 +0200

    FEAT: added report creation endpoint

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Serialize(ReportDto)
+  @Post()
+  create(
+    @Body() createReportDto: CreateReportDto,
+    @CurrentUser() currentUser: User,
+  ) {
+    return this.reportsService.create(createReportDto, currentUser);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private repository: Repository<Report>,
+  ) {}
+
+  async create(
+    createReportDto: CreateReportDto,
+    currentUser: User,
+  ): Promise<Report> {
+    const report = this.repository.create(createReportDto);
+    report.user = currentUser;
+    return this.repository.save(report);
+  }
+}
