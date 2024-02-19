5c68494eee9ed860add3cf68a43dd3bfd5fb6a67
Bohdan <bohdan.duda.it@gmail.com>
2024-02-19 22:24:23 +0200

    FEAT: added get estimate endpoint

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -16,6 +18,7 @@ import { ReportDto } from './dtos/report.dto';
 import { ChangeApprovalDto } from './dtos/change-approval.dto';
 import { AdminGuard } from '../guards/admin.guard';
 import { AuthGuard } from '../guards/auth.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @UseGuards(AuthGuard)
 @Controller('reports')
@@ -38,5 +41,10 @@ export class ReportsController {
     @Body() changeApprovalDto: ChangeApprovalDto,
   ) {
     return this.reportsService.changeApproval(id, changeApprovalDto.approved);
+  }
+
+  @Get('estimate')
+  getEstimate(@Query() getEstimateDto: GetEstimateDto) {
+    return this.reportsService.getEstimate(getEstimateDto);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -24,5 +25,23 @@ export class ReportsService {
     const report = await this.repository.findOneBy({ id });
     report.approved = approved;
     return this.repository.save(report);
+  }
+
+  async getEstimate({
+    make,
+    model,
+    year,
+    lng,
+    lat,
+  }: GetEstimateDto): Promise<{ price: number }> {
+    return this.repository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .getRawOne();
   }
 }
